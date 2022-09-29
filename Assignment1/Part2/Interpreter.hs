27fee7e5f90309d9342db0ade359aad69ce5b83b
ryrysmiley <>
2022-09-29 15:54:08 -0700

    adjustments to negative intepreter

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -4,7 +4,8 @@ import AbsNumbers
 
 eval :: Exp -> Integer
 eval (Num n) = n
-eval (Neg n) = (eval n) * (-1)
+eval (Neg n) = negate (eval n)
+eval (Paren n) = (eval n)
 eval (Plus n m) = (eval n) + (eval m)
 eval (Minus n m) = (eval n) - (eval m)
 eval (Times n m) = (eval n) * (eval m)
