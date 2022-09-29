92e9ba8ceef3edd23bc8cbc226d4eee12b7699c1
ryrysmiley <>
2022-09-29 16:37:59 -0700

    absolute value interpreter

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -5,6 +5,7 @@ import AbsNumbers
 eval :: Exp -> Integer
 eval (Num n) = n
 eval (Neg n) = negate (eval n)
+eval (Abs n) = abs (eval n)
 eval (Paren n) = (eval n)
 eval (Plus n m) = (eval n) + (eval m)
 eval (Minus n m) = (eval n) - (eval m)
